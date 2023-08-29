bf3348860b80bd538e0b3404a21a51964f1c2fdc
yangmingzi <yangmingzi@wondersgroup.com>
2023-08-29 15:17:59 +0800

    merge

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/test", function (req, res, next) {
+  console.log(req);
+  res.send({
+      resultMsg: null,
+      result: `好看不？，陈桔`,
+      code: 200,
+  });
+});
 module.exports = router;
